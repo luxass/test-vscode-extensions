0a6e74c88255bf7ff30fe372b3ae106789cb6bdd
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-18 08:58:59 +0200

    fix: bundle

--- pnpm/tsup.config.ts
+++ pnpm/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   format: ["cjs"],
   clean: true,
   treeshake: true,
-  bundle: false,
+  bundle: true,
   target: ["node18", "es2022"],
   platform: "node",
   external: ["vscode"],
